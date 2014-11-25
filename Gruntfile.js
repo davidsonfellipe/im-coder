5e91b240611fea24d1d62dd2500a3573f1dbec88
Davidson Fellipe <email@fellipe.com>
2014-11-25 04:19:19 -0200

    task grunt-ftpush

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,41 @@ module.exports = function( grunt ) {
     "use strict";
     grunt.initConfig({
         jshint: {
-            all: ['Gruntfile.js','assets/js/script.js']
+            all: ['app/assets/js/script.js']
         },
         connect: {
             server: {
                 options: {
                     port: 9000,
                     hostname: '*',
-                    base: '.',
+                    base: 'app/',
                     open: true,
                     keepalive: true
                 }
             }
+        },
+        ftpush: {
+          build: {
+            auth: {
+              host: 'fellipe.com',
+              port: 21,
+              authKey: 'key'
+            },
+            src: 'app/',
+            dest: '/public_html/apps/im-coder/',
+            exclusions: ['.DS_Store',
+                         'Makefile',
+                         'package.json',
+                         'Gruntfile.js',
+                         'node_modules'],
+            simple: true,
+            useList: false
+          }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-ftpush');
     grunt.registerTask('default', ['jshint']);
 };
